73b9624e3cfe42b54a1212d4f369b07f39b18190
kenny-nzungu <nzungukenedy@gmail.com>
2025-03-10 19:21:00 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,1 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
-
-// Middleware to serve static files (like your HTML, CSS, JS)
-app.use(express.static('public')); // Change to your folder if different
-
-// Example API route
-app.get('/api/message', (req, res) => {
-    res.json({ message: 'Hello from Express backend!' });
-});
-
-// Start the server
-app.listen(PORT, () => {
-    console.log(
-
+wdfyu
